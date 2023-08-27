209427b0557c4cca1dbad42c932eff95cb75c6f9
reformedhope <steph_salzburg@hotmail.com>
2023-08-27 14:01:41 -0400

    create my first app in c#

---
+++ Learning_CSharp/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Learning_CSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName;
+            int age;
+
+           Console.WriteLine("What is your name?");
+           firstName = Console.ReadLine();
+           Console.WriteLine($"Hello my name is, {firstName}");
+            // doing cw tab tab should give you the write line
+            Console.WriteLine("What is your age");
+            age = Convert.ToInt32 (Console.ReadLine());
+                if (age >= 16) { 
+            Console.WriteLine($"Hello my name is {firstName} and I am {age} I  can drive");
+            }
+            else   
+                    {
+            Console.WriteLine($"Hello my name is {firstName} and I am {age} I  cant drive");
+            }
+
+
+        }
+    }
+}
