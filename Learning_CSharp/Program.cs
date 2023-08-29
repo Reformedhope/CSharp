66accb3a94b5ee08101584994a872e18f2ffca79
Steph <steph_salzburg@hotmail.com>
2023-08-28 21:05:29 -0400

    updated string

--- Learning_CSharp/Program.cs
+++ Learning_CSharp/Program.cs
@@ -5,21 +5,34 @@
         static void Main(string[] args)
         {
             string firstName;
+            string lastName;
             int age;
 
-           Console.WriteLine("What is your name?");
-           firstName = Console.ReadLine();
-           Console.WriteLine($"Hello my name is, {firstName}");
+           Console.WriteLine("What is your first name?");
+            firstName = Console.ReadLine();
+            Console.WriteLine("What is your last name?");
+            lastName = Console.ReadLine();
+
+            Console.WriteLine($"Hello my name is, {firstName}  {lastName}");
             // doing cw tab tab should give you the write line
             Console.WriteLine("What is your age");
+
             age = Convert.ToInt32 (Console.ReadLine());
                 if (age >= 16) { 
             Console.WriteLine($"Hello my name is {firstName} and I am {age} I  can drive");
             }
             else   
                     {
-            Console.WriteLine($"Hello my name is {firstName} and I am {age} I  cant drive");
+            Console.WriteLine($"Hello my name is {firstName} {lastName}and I am {age} I  cant drive");
             }
+
+
+
+
+
+
+
+
 
 
         }
